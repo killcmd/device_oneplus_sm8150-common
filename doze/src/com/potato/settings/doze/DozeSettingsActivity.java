5c0a381e62d34aab2c0494ed46429593e75c193e
Ferdinand Vasquez <killcmd@gmail.com>
2020-03-14 17:21:03 -0400

    Update DozeSettingsActivity.java

--- doze/src/com/potato/settings/doze/DozeSettingsActivity.java
+++ doze/src/com/potato/settings/doze/DozeSettingsActivity.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.potato.settings.doze;
+package xxx.killcmd.settings.doze;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
